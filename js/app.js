4d04ab951994fde9a53823b8ab4ffd69bb45923b
Thomas Sherer <olc0710@yahoo.com>
2019-11-13 22:05:11 -0800

    Lab 6 corrections

--- js/app.js
+++ js/app.js
@@ -34,31 +34,33 @@ var storeSeattle = {
   calculateCookieCount: function() {
     for(var i = 0; i < this.customersEachHourArray.length; i++) {
       var cookieCount = Math.round(this.customersEachHourArray[i] * this.averageCookiesPerCustomer);
-      this.customersEachHourArray.push(cookieCount);
+      this.cookiesSoldEachHourArray.push(cookieCount);
     }
   }, //Closes cookies/hr method
 
   // Render function to pass values to Sales page
   render:  function() {
+    this.generateCustPerHour();
+    this.calculateCookieCount();
     var ulElement = document.createElement('ul');
     var h2Element = document.createElement('h2');
     h2Element.textContent = this.storeName;
     storeSales.appendChild(h2Element);
     for( var i = 0; i < hours.length; i++ ) {
       var listElement = document.createElement('li');                         //give element content
-      listElement.textContent = `${hours[i]}: ${this.cookiesSoldEachHour[i]}`; //append to DOM
+      listElement.textContent = `${hours[i]}: ${this.cookiesSoldEachHourArray[i]}`; //append to DOM
+      ulElement.appendChild(listElement);
+      // } //Closes For Loop
       storeSales.appendChild(ulElement);
-    } //Closes For Loop
-  } //Closes function
-  storeSales.appendChild(ulElement);
-}; //Closes object
-
+    }//Closes function
+  } //Closes object
+};
 //Random number generator - generates a value between min and max, inclusive of min and max
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min +1)) + min;
 }
 
-// seattle.render(23, 65);
+storeSeattle.render();
 
 
 // var storeTokyo = {
